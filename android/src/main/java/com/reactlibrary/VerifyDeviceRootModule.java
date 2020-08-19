dbcba8a84b704d93d5de9a5e7f25f47f965f161a
Gleidson Daniel Silva <gleidson10daniel@hotmail.com>
2020-08-18 21:37:26 -0300

    Update VerifyDeviceRootModule.java
    Fix Promise import

--- android/src/main/java/com/reactlibrary/VerifyDeviceRootModule.java
+++ android/src/main/java/com/reactlibrary/VerifyDeviceRootModule.java
@@ -4,6 +4,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 
 import com.scottyab.rootbeer.RootBeer;
 
