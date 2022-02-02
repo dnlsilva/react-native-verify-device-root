1f4728ecb4d5fc844a078faaaf81f8a4750d8bb2
Gleidson Daniel Silva <gleidson10daniel@hotmail.com>
2022-02-02 08:33:41 -0300

    fix name

--- android/src/main/java/com/reactlibrary/VerifyDeviceRootModule.java
+++ android/src/main/java/com/reactlibrary/VerifyDeviceRootModule.java
@@ -8,11 +8,11 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
 
-@ReactModule(name = AwesomeModuleModule.NAME)
-public class AwesomeModuleModule extends ReactContextBaseJavaModule {
+@ReactModule(name = VerifyDeviceRootModule.NAME)
+public class VerifyDeviceRootModule extends ReactContextBaseJavaModule {
     public static final String NAME = "VerifyDeviceRoot";
 
-    public AwesomeModuleModule(ReactApplicationContext reactContext) {
+    public VerifyDeviceRootModule(ReactApplicationContext reactContext) {
         super(reactContext);
     }
 
